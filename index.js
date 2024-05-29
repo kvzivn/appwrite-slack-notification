24de075298f8f742b3f0aae6f634a3c91d7e2069
Kevin <hello@kevinivan.com>
2024-05-30 00:51:18 +0200

    req req

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
 import axios from "axios"
 
 export default async (req, res) => {
-  let payload
+  console.log("Request received:", req)
+  const body = req.req.bodyRaw || req.req.body
+  const { email, firstName, lastName } =
+    typeof body === "string" ? JSON.parse(body) : body
+  const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL
+  const message = `Someone applied to join the network!\n- Name: ${firstName} ${lastName}\n- Email: ${email}`
 
   try {
-    if (req.method === "POST") {
-      payload = typeof req.body === "string" ? JSON.parse(req.body) : req.body
-    } else {
-      payload = req.payload ? JSON.parse(req.payload) : {}
-    }
-
-    console.log("Parsed payload:", payload)
-
-    const document = payload.$id ? payload : payload.events?.[0]
-    const email = document.email || (document.payload && document.payload.email)
-    const firstName =
-      document.firstName || (document.payload && document.payload.firstName)
-    const lastName =
-      document.lastName || (document.payload && document.payload.lastName)
-
-    console.log("Extracted email:", email)
-
-    const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL
-    const message = `Someone applied to join the network!\n- Name: ${firstName} ${lastName}\n- Email: ${email}`
     await axios.post(slackWebhookUrl, {
       text: message,
     })
+
+    res.json({
+      success: true,
+      message: "Notification sent to Slack!",
+    })
   } catch (error) {
-    console.error(error)
+    res.json({
+      success: false,
+      message: "Failed to send notification to Slack!",
+      error: error.message,
+    })
   }
 }
